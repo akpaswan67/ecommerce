3b18405fd269fcc79ecb723298dee1c52f3e773c
Arun Kumar Paswan <arunkumarpaswn38@gmail.com>
2023-08-18 00:05:42 +0530

    adding changes

--- server/controller/product-controller.js
+++ server/controller/product-controller.js
@@ -5,7 +5,7 @@ import Product from '../model/product-schema.js';
 export const getProducts=async(request,response)=>{
 
 	try{
-		const products =await Product.find({});
+		const products =await Product.find();
 		response.status(200).json(products);
 	}catch(error)
 	{
