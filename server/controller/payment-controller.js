79f8316cbfedaf98dd41ef7ec8b88b2a48813c19
Arun Kumar Paswan <arunkumarpaswn38@gmail.com>
2023-08-17 23:26:23 +0530

    adding project

---
+++ server/controller/payment-controller.js
@@ -0,0 +1,17 @@
+import paytmchecksum from '../paytm/PaytmChecksum.js';
+import { paytmParams,paytmMerchantKey } from '../index.js';
+
+export const addPaymentGateway =async(request,response)=>{
+	try{
+	let paytmChecksum= await paytmchecksum.generateSignature(paytmParams,paytmMerchantKey);
+
+	let params ={
+		...paytmParams,'CHECKSUMHASH':paytmChecksum
+	}
+
+	response.status(200).json(params);
+	}catch(error)
+	{
+		response.status(500).json({error:error.message});
+	}
+}
